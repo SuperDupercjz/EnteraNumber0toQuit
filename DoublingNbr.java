952954247e9185bb0c37ada69568e1693ef448b7
CJ <ruachobeach@gmail.com>
2021-02-03 11:04:21 -0700

    0.3

--- DoublingNbr.java
+++ DoublingNbr.java
@@ -6,7 +6,7 @@ public class DoublingNbr{
  double originalNumber;
  double calculatedNumber;
  
- // Get a number from a user
+ // Get a number from a user to play the game
  System.out.print("Enter a number: ");
  originalNumber = keyboard.nextDouble();
  while (originalNumber != 0){
@@ -15,7 +15,7 @@ public class DoublingNbr{
     //display the results to the user
     System.out.println( originalNumber + " X 2 = " + calculatedNumber);
     // ask the user for another number or sentinal 0 to quit
-     System.out.print("Enter a number or 0 to quit: ");
+     System.out.print("Enter a number or 0 to quit game: ");
  originalNumber = keyboard.nextDouble();
  } // end while
  System.out.println(" Thanks for playing!");
