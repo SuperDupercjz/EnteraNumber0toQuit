710595c3e4efec8b246fd1e0d3c8024367a6b223
CJ <ruachobeach@gmail.com>
2021-02-03 10:52:22 -0700

    some java

---
+++ DoublingNbr.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class DoublingNbr{
+ public static void main(String[] args){
+ //declaration
+ Scanner keyboard = new Scanner(System.in);
+ double originalNumber;
+ double calculatedNumber;
+ 
+ // Get a number from a user
+ System.out.print("Enter a number: ");
+ originalNumber = keyboard.nextDouble();
+ while (originalNumber != 0){
+    //double the number
+    calculatedNumber = originalNumber * 2;
+    //display the results to the user
+    System.out.println( originalNumber + " X 2 = " + calculatedNumber);
+    // ask the user for another number or sentinal 0 to quit
+     System.out.print("Enter a number or 0 to quit: ");
+ originalNumber = keyboard.nextDouble();
+ } // end while
+ System.out.println(" Thanks for playing!");
+ }//end main
+}//end class
